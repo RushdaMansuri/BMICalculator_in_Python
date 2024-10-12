c086be6a89f3c797cdce8ff25638a06e49ccfc51
Rushda Mansuri <101844644+RushdaMansuri@users.noreply.github.com>
2024-10-11 20:11:11 -0400

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,14 @@
+import customtkinter
+from tkinter import *
+from tkinter import messagebox
+
+app = customtkinter.CTk()
+app.title("BMI Calculator")
+app.geometry("300x350")
+app.config(bg="#000")
+
+font1 = ("Arial", 30, "bold")
+font2 = ("Arial", 30, "bold")
+font3 = ("Arial", 30, "bold")
+
+app.mainloop()
